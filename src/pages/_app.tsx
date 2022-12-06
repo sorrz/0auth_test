7feac8b35757da2283771d0a5ea478485d9b553f
Dan Stjärnborg <dan.stjarnborg@gmail.com>
2022-12-06 09:38:04 +0100

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+import { trpc } from "../utils/trpc";
+
+import '../components/businesscard/BusinessCard.css'
+import "../styles/globals.css";
+import Navbar from "../components/shared/navbar";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Navbar />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default trpc.withTRPC(MyApp);
